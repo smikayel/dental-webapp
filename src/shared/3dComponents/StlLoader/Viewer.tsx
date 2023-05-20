5d5f885f44c870ad1f3ce8b940d30c40feb31561
smikayel <sasha-mikayelyan@mail.ru>
2023-05-20 23:19:14 +0400

    add stl loader, add obtain controls

--- src/shared/3dComponents/StlLoader/Viewer.tsx
+++ src/shared/3dComponents/StlLoader/Viewer.tsx
@@ -1,51 +1,72 @@
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-import React, { useEffect, useRef } from 'react';
+import { STLLoader as Loader } from 'three/examples/jsm/loaders/STLLoader';
+import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Import OrbitControls
 import * as THREE from 'three';
-// import * from '
+import styles from './style.module.css'
 
-const Viewer = ({ stlUrl = '../../../assets/models/JawModel/Jaw.stl' }) => {
+const rendererStl = (
+  containerRef: React.RefObject<HTMLDivElement>,
+  stlUrl: string,
+  setCamera:  Dispatch<SetStateAction<THREE.PerspectiveCamera | undefined>>
+) => {
+  const loader = new Loader();
+  const container = containerRef.current;
+  if (!container) return;
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 15000);
+  camera.position.z = 5;
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
+  loader.load(stlUrl, geometry => {
+    console.log(stlUrl)
+    const material = new THREE.MeshNormalMaterial();
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    renderer.render(scene, camera);
+  });
+
+  setCamera(camera)
+  return () => {
+    container.removeChild(renderer.domElement);
+  };
+}
+
+const AddController = (
+  containerRef: React.RefObject<HTMLDivElement>,
+  setObtainControlls: Dispatch<SetStateAction<OrbitControls>>,
+  camera?: THREE.PerspectiveCamera,
+) => {
+
+  const container = containerRef.current;
+  if (!container || !camera) return 
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  container.appendChild(renderer.domElement);
+
+  // Create OrbitControls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+  setObtainControlls(controls)
+}
+
+const Viewer = ({ stlUrl = 'assets/models/JawModel/Jaw.stl', width = window.innerWidth, height = window.innerHeight }) => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [camera, setCamera] = useState<THREE.PerspectiveCamera>()
+  const [obtainControlls, setObtainControlls] = useState<SetStateAction<OrbitControls>>()
 
   useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    // Create a scene
-    const scene = new THREE.Scene();
-
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    camera.position.z = 5;
-
-    // Create a renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(container.clientWidth, container.clientHeight);
-    container.appendChild(renderer.domElement);
-
-    // Create a STL loader
-    const loader = new STLLoader();
-    loader.load(stlUrl, geometry => {
-      // Create a material
-      const material = new THREE.MeshNormalMaterial();
-
-      // Create a mesh and add it to the scene
-      const mesh = new THREE.Mesh(geometry, material);
-      scene.add(mesh);
-
-      // Render the scene
-      renderer.render(scene, camera);
-    });
-
-    // Clean up on unmount
-    return () => {
-      container.removeChild(renderer.domElement);
-    };
+    // load and render Jaw model
+    rendererStl(containerRef, stlUrl, setCamera)
+    // load controller
+    AddController(containerRef, setObtainControlls, camera)
   }, [stlUrl]);
 
   return ( 
       <>
-        <div>Hello</div>
-        <div ref={containerRef} style={{ width: '100%', height: '100%' }} />
+        <div ref={containerRef} style={{ width: width, height: height }} className={styles.Viewer}/>
       </>
     )
 };
